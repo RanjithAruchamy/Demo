ddf5eaf6d6956cc5186a66a157e92a7dba2ec672
RanjithAruchamy <ranjith191098@gmail.com>
2021-09-28 19:53:48 +0530

    dev commit

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express =  require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const bodyParser = require('body-parser');
-const router = require('./router/routes');
-const path = require('path');
-
+const bodyParser = require("body-parser");
+const router = require("./router/routes");
+const path = require("path");
 
 app.use(bodyParser.json());
-app.use('/api', router);
+app.use("/api", router);
 
-app.use('/images', express.static(path.join(__dirname, '/public/files')))
-app.use("/", express.static(path.join(__dirname, '/Frontend')))
+app.use("/images", express.static(path.join(__dirname, "/public/files")));
+app.use("/", express.static(path.join(__dirname, "/Frontend")));
 // app.use(path.join(__dirname + '/Frontend', 'app.html'))
-mongoose.connect("mongodb+srv://adminUser:Admin123@ranjithcluster.jkhq5.mongodb.net/demo?retryWrites=true&w=majority",{ useUnifiedTopology: true, useNewUrlParser: true,useFindAndModify: false }, 
-() => {
+mongoose.connect(
+  "mongodb+srv://adminUser:Admin123@ranjithcluster.jkhq5.mongodb.net/demo?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false },
+  () => {
     console.log("database connected");
-})
+  }
+);
 
 app.listen(8000, () => console.log("server started"));
